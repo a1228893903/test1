79c991a5ae175c5126c8f98ab090d02827a6fb9b
a1228893903 <a1228893903@qq.com>
2018-05-04 10:00:15 +0800

    第二次修改

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+int add(int a, int b, int c)
+{
+   return a + b + c;
+}
+
 int main()
 {
-   int a = 1;
-   int b = 9;
-   return a + ;
+   int m = add(1,2,3);
+   printf("%d\n",m);
+   return 0 ;
 }
